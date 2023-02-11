9517d1fe5f90ff2f89da5740691f31ea22aeaaaa
hafsa <hfsyln93@gmail.com>
2023-02-11 23:36:59 +0300

    add login page

---
+++ src/firebaseConfig.js
@@ -0,0 +1,53 @@
+
+import { initializeApp } from "firebase/app";
+import {getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signOut} from "firebase/auth";
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBJSpBhJD4btm0kk01Dz7hvB_HRhWkDGkw",
+  authDomain: "confysloth.firebaseapp.com",
+  projectId: "confysloth",
+  storageBucket: "confysloth.appspot.com",
+  messagingSenderId: "633846364417",
+  appId: "1:633846364417:web:48e297aaaa5b31287e4300"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const provider = new GoogleAuthProvider()
+
+export const signInWithGoogle = () => {
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      console.log(result)
+      // const name = result.user.displayName;
+      // const email = result.user.email;
+      // const profilePic = result.user.photoURL;
+      }).catch((error) =>{
+        console.log(error)
+      })
+    }
+
+export const auth = getAuth(app)
+
+
+
+export const userObserver = (setCurrentUser) => {
+  //? Kullanıcının signin olup olmadığını takip eden ve kullanıcı değiştiğinde yeni kullanıcıyı response olarak dönen firebase metodu
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      const {email, displayname, photoURL} = user
+      setCurrentUser({email, displayname, photoURL})
+      console.log(user);
+    } else {
+      console.log("user signed out");
+      setCurrentUser(false)
+    }
+  });
+};
+
+export const logOut = () => {
+  signOut(auth);
+ 
+};
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,8 +1,61 @@
 import React from 'react'
+import { signInWithEmailAndPassword,} from 'firebase/auth';
+import { auth, signInWithGoogle } from '../firebaseConfig';
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import Profil from "../assets/profil.webp"
+import { Avatar, Box, Button } from '@mui/material';
+
+
 
 const LoginPage = () => {
+
+  const navigate = useNavigate();
+    
+    const [loginEmail, setLoginEmail] = useState("")
+    const [loginPass, setLoginPass] = useState("")
+
+   const login= async () =>{
+    try {
+    const user = await signInWithEmailAndPassword(auth, loginEmail, loginPass)
+    console.log(user)
+    navigate("/")
+    }catch(error){
+        console.log(error)
+        alert("Geçerli bir giriş yapınız yada kayıt olunuz")
+    }
+   }
+
+
+const signInGoog = () => {
+  signInWithGoogle()
+  navigate("/")
+}
+ 
+
+
+
   return (
-    <div>LoginPage</div>
+   
+      
+          <Box sx={{ width:"25%", margin:"auto", display:"flex", flexDirection:"column", gap:"2rem", alignItems:"center", backgroundColor:"orange", my:"3rem"}}>
+          <Avatar src={Profil} sx={{width:"10rem", height:"10rem", mt:"2rem"}}/>
+          <Box sx={{mx:"5",  display:"flex", flexDirection: "column", gap:"1rem"}} >
+              <label className="form-label">Email address:</label>
+              <input  value={loginEmail} type="text" onChange={(e)=> {setLoginEmail(e.target.value)} }/>
+          </Box>
+          <Box sx={{mx:"5",  display:"flex", flexDirection: "column", gap:"1rem"}}>
+              <label  className="form-label">Password</label>
+              <input value={loginPass} type="password" onChange={(e)=> {setLoginPass(e.target.value)}}/>
+          </Box>
+          <Button  onClick={login}>login</Button>
+          <Button  onClick={signInGoog}>Sing In with Google</Button>
+          </Box>
+          
+        
+
+
+        
   )
 }
 
