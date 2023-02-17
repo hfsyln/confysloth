9d6b9067a4dfb5e45cf58c2c7f09d0979ef27518
hafsa <hfsyln93@gmail.com>
2023-02-17 23:00:24 +0300

    cartDetail add

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ import Register from './pages/Register';
 import CardPage from './pages/CardPage';
 import Product from './components/Product';
 import ErrorPage from './pages/ErrorPage';
-import NavBar from "./components/NavBar"
-import Footer from "./components/Footer"
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import CartDetail from './components/cartDetail';
 
 
 function App() {
@@ -17,13 +18,14 @@ function App() {
       <BrowserRouter>
       <NavBar/>
       <Routes>
-      <Route path='/' element={<HomePage/>}/>
-      <Route path='/login' element={<LoginPage/>}/>
-      <Route path='/register' element={<Register/>}/>
-      <Route path='/about' element={<AboutPage/>}/>
-        <Route path='/cart' element={<CardPage />} />
-        <Route path='/product' element={<Product />} />
-        <Route path='/error' element={<ErrorPage />} />
+          <Route path='/' element={<HomePage/>}/>
+          <Route path='/login' element={<LoginPage/>}/>
+          <Route path='/register' element={<Register/>}/>
+          <Route path='/about' element={<AboutPage/>}/>
+          <Route path='/cart' element={<CardPage />} />
+          <Route path='/product' element={<Product />} />
+          <Route path='/error' element={<ErrorPage />} />
+          <Route path='/detail' element={<CartDetail/>} />
       </Routes>
       <Footer/>
     </BrowserRouter>
